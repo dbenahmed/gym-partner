be9eed0d010faa256581b326a90fe438a554bfc5
Djilali Benahmed <dbenahmedd@gmail.com>
2025-03-18 01:36:58 +0100

    app.use every router inside routes folder ( ai gen )

--- server/app.js
+++ server/app.js
@@ -5,6 +5,18 @@ import dotenv from "dotenv";
 import collectionsRouter from "./routes/workoutCollectionsRoutes.js";
 import authRouter from "./routes/authRoutes.js"
 import plansRouter from "./routes/workoutPlansRoutes.js";
+import workoutTemplatesRouter from './routes/workoutTemplatesRoutes.js';
+import workoutSessionsRouter from './routes/workoutSessionsRoutes.js';
+import workoutHistoryRouter from './routes/workoutHistoryRoutes.js';
+import weightTrackingRouter from './routes/weightTrackingRoutes.js';
+import nutritionGoalsRouter from './routes/nutritionGoalsRoutes.js';
+import mealDatabaseRouter from './routes/mealDatabaseRoutes.js';
+import mealVotingRouter from './routes/mealVotingRoutes.js';
+import foodTrackingRouter from './routes/foodTrackingRoutes.js';
+import exerciseDatabaseRouter from './routes/exerciseDatabaseRoutes.js';
+import dashboardRouter from './routes/dashboardRoutes.js';
+import customFoodTrackingRouter from './routes/customFoodTrackingRoutes.js';
+import adminDashboardRouter from './routes/adminDashboardRoutes.js';
 
 dotenv.config();
 
@@ -21,6 +33,18 @@ app.use(cookieParser())
 app.use(collectionsRouter);
 app.use(authRouter);
 app.use(plansRouter)
+app.use(workoutTemplatesRouter);
+app.use(workoutSessionsRouter);
+app.use(workoutHistoryRouter);
+app.use(weightTrackingRouter);
+app.use(nutritionGoalsRouter);
+app.use(mealDatabaseRouter);
+app.use(mealVotingRouter);
+app.use(foodTrackingRouter);
+app.use(exerciseDatabaseRouter);
+app.use(dashboardRouter);
+app.use(customFoodTrackingRouter);
+app.use(adminDashboardRouter);
 
 app.get('/', async (req, res) => {
 
