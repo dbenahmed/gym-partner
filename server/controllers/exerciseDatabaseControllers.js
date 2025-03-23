08f7eb112dc79d41a8fadaa2fa05eb3cf1229d44
BENAHMED Djilali <66162420+dbenahmed@users.noreply.github.com>
2025-03-23 23:01:43 +0100

    Feature search exercises (#21)
    * adding logger for drizzle,
    making the search exercises controller
    
    * adding count, limit to the search exercises by filtering, making the get exercise details controller
    
    ---------
    
    Co-authored-by: Djilali Benahmed <dbenahmedd@gmail.com>

--- server/controllers/exerciseDatabaseControllers.js
+++ server/controllers/exerciseDatabaseControllers.js
@@ -1,18 +1,72 @@
+import db from "../db/index.js";
+import { collections, exercises, plans, users } from "../db/schemas/dev/schema.js";
+import { and, eq } from "drizzle-orm";
+
 // Get a list of all available exercises
-export const getAllExercises = (req, res) => {
+export const getAllExercises = async (req, res) => {
   try {
     const userId = req.user;
-    // ... existing code ...
+    const userData = req.userData
+    const queries = {
+      "force": req.query.force,
+      "level": req.query.level,
+      "mechanic": req.query.mechanic,
+      "equipment": req.query.equipment,
+      "primarymuscles": req.query.primarymuscles,
+      "secondarymuscles": req.query.secondarymuscles,
+      "category": req.query.category,
+    }
+    const { page } = req.query
+    const limit = (req.query.limit) ? parseInt(req.query.limit) : 10;
+
+    const filteredQueries = Object.fromEntries(Object.entries(queries).filter((v, i) => v[1] !== ""))
+
+    console.log('filteredQueries', filteredQueries)
+    const andConditions = Object.entries(filteredQueries).map(([key, value]) => {
+      return eq(exercises[key], value)
+    })
+    const foundExercises = await db.select({
+      ...exercises,
+    }).from(exercises).where(
+      and(
+        ...andConditions
+      )
+    ).limit(limit).offset(page)
+
+    const count = await db.$count(exercises, and(...andConditions))
+
+    res.status(200).json({
+      success: true, data: {
+        count,
+        exercises: foundExercises
+      }
+    })
   } catch (error) {
+    console.error(error)
     res.status(500).json({ message: 'Error retrieving all exercises', error: error.message });
   }
 };
 
 // Get details for a specific exercise
-export const getExerciseDetails = (req, res) => {
+export const getExerciseDetails = async (req, res) => {
   try {
     const userId = req.user;
-    // ... existing code ...
+    const { exerciseId } = req.params
+
+    const foundExercise = await db.select().from(exercises).where(eq(exercises.id, exerciseId)).limit(1)
+
+    if (!foundExercise || foundExercise.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'Exercise Not Found'
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: foundExercise[0]
+    })
+
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving exercise details', error: error.message });
   }
@@ -33,8 +87,8 @@ export const updateExercise = (req, res) => {
     const userId = req.user;
     const { exerciseId } = req.params;
     const updatedData = req.body;
-    
-    
+
+
     res.status(200).json({ message: 'Exercise updated successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Error updating exercise', error: error.message });
--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import * as devSchema from "./schemas/dev/schema.js"
 import * as devRelations from "./schemas/dev/relations.js"
 dotenv.config();
-const db = drizzle(process.env.SUPABASE_DATABASE_URL, { schema: { ...devSchema, ...devRelations } })
+
+const db = drizzle(process.env.SUPABASE_DATABASE_URL, { schema: { ...devSchema, ...devRelations }, logger: true })
 
 export default db
