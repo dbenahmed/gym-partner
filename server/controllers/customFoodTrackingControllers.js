9f3fd17ff03a9d54d95b8cf3dda034c8d2affeea
abdelhakim <bendjelloulabedelhakim@gmail.com>
2025-04-09 19:35:05 +0100

    changes in the customFoodTracking File

--- server/controllers/customFoodTrackingControllers.js
+++ server/controllers/customFoodTrackingControllers.js
@@ -1,16 +1,20 @@
+import { eq } from "drizzle-orm";
 
 // Create a new custom meal
 export const createCustomMeal = async (req, res) =>{
     try {
     const  {foodname  , calories,  proteinper100g ,carbohydratesper100g, fatper100g } = req.body ;
-    const existMeal = await db.select().from(foods).where(eq(foods.name,name)).limit(1);
+//    check if the meal is already exist 
+
+    const existMeal = await db.select().from(foods).where(eq(foods.name,foodname)).limit(1);
     if (existMeal.length > 0 )
 {
- res.status(400).json({
+  return res.status(400).json({
     succes:false,
     message : "this meals is already exist"
- })  
-}else{
+ })}  
+{
+    const userID = req.user.id; 
     const result = await db.insert(foods).values({
         foodname,
         calories,
@@ -18,7 +22,7 @@ export const createCustomMeal = async (req, res) =>{
         carbohydratesper100g,
         fatper100g,
         custom:true,
-        created_by:user.id, 
+        created_by:userID, 
     })
     res.status(201).json({
      message : "the meals is added succesfuly",
@@ -27,14 +31,36 @@ export const createCustomMeal = async (req, res) =>{
 } } 
 catch (err){
  res.json ({
-    succes : false,
+    success : false,
     message : err.message
  });
 }};
 
 // Get a list of all custom meals created by the user
-export const getCustomMeals = (req, res) => {
+export const getCustomMeals = async (req, res) => {
+    try{
+   const {foodname}= req.body;
+   const ExistFood = await db.select().from(foods).where(eq(foods.foodname,foodname));
+  if (ExistFood.length === 0){
 
+   return res.status(404).json({
+    success:false,
+        message : "this meal is not exist"
+    })
+  }
+  {
+    return res.status(200).json({
+        success:true,
+        meal : ExistFood[0]
+    })
+  }
+}
+catch (err) {
+   res.status(500).json({
+    success:false,
+    message:err.message
+   })
+}
 };
 
 // Update a custom meal
