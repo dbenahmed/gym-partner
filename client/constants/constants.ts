01fb3f658675cac124fa9680da42b6930f5cca9c
Djilali Benahmed <dbenahmedd@gmail.com>
2025-04-24 22:45:54 +0100

    made a request traffic forwarding from serveo to localhost

--- client/constants/constants.ts
+++ client/constants/constants.ts
@@ -2,5 +2,5 @@ import Constants from "expo-constants";
 const url = Constants.expoConfig?.extra?.BACKEND_URL;
 const port = Constants.expoConfig?.extra?.PORT;
 
-export const defaultUrl = `${url}:${port}`;
-
+// export const defaultUrl = `${url}:${port}`;
+export const defaultUrl = `${url}`;
