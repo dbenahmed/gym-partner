35d134e75503d7f622267282fce57eb4c054be88
Djilali Benahmed <dbenahmedd@gmail.com>
2025-06-29 19:04:59 +0100

    bug fixing drizzle connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 // POSTGRESQL CONNECTION
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { config } from "../config/env.js"
+import * as schema from "../db/schemas/schema.js"
 
-
-const db = drizzle(config.databaseUrl, { logger: true })
+const db = drizzle(config.databaseUrl, { schema }, { logger: true })
 
 export default db
