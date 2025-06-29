5815ce8c58c7e4384cbdd08fe401773f5b1765e0
Djilali Benahmed <dbenahmedd@gmail.com>
2025-06-30 00:22:18 +0100

    rendering the weights and adding new weights finished frontend

--- client/app/(protected)/Profile/body-weight-tracking.jsx
+++ client/app/(protected)/Profile/body-weight-tracking.jsx
@@ -1,45 +1,181 @@
 import useThemeContext from "@/context/themeContext";
-import React, { useState } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet, Alert, Modal, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
 import Button from "@/components/ui/Button";
+import { defaultUrl } from "@/constants/constants";
+import ModalSlideUp from "@/components/ui/ModalSlideUp";
+import useAuth from "@/context/authContext";
 
 
 export default function BodyWeightTrackingScreen() {
 
     const { colors, theme } = useThemeContext();
-    const fakeData = Array.from({ length: 50 }, (_, index) => ({
-        id: index.toString(),
-        weight: Math.floor(Math.random() * 50) + 50, // Random weight between 50-100
-        height: Math.floor(Math.random() * 50) + 150, // Random height between 150-200
-    }));
+    const { authenticated } = useAuth();
 
-    const [data, setData] = useState(fakeData);
+    const [isVisible, setIsVisible] = useState(false);
+    const [weights, setWeights] = useState([]);
+    const [unit, setUnit] = useState("kg");
+    const [weight, setWeight] = useState(null);
+    const fetchWeights = async () => {
+
+        try {
+            const response = await fetch(`${defaultUrl}/weight`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authenticated}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Failed to fetch weights');
+            }
+            const { success, data, message } = await response.json();
+            if (!success) {
+                throw new Error(message);
+            }
+            console.log(data)
+            setWeights(data);
+        } catch (error) {
+            console.error('Error fetching weights:', error);
+            Alert.alert('Error', error.message);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchWeights();
+    }, []);
+
+
+    const deleteWeight = async (id) => {
+        try {
+            console.log(id)
+            const response = await fetch(`${defaultUrl}/weight/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authenticated}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Failed to delete weight');
+            }
+            const { success, message } = await response.json();
+            if (!success) {
+                throw new Error(message);
+            }
+            Alert.alert('Success', 'Weight deleted successfully');
+            fetchWeights();
+        } catch (error) {
+            console.error('Error deleting weight:', error);
+            Alert.alert('Error', error.message);
+        }
+    }
 
     const renderItem = ({ item }) => (
-        <View style={[styles.listItem, { backgroundColor: colors.tintLighter }]}>
+        <View style={[styles.listItem, { backgroundColor: colors.tintLighter, flexDirection: 'row', justifyContent: 'space-between' }]}>
             <Text style={[styles.listItemText, { color: colors.text }]}>
-                Weight: {item.weight} kg
+                Weight: {item.weight} {item.unit}
             </Text>
-            <Text style={[styles.listItemText, { color: colors.text }]}>
-                Height: {item.height} cm
-            </Text>
+            <Button
+                onPress={() => {
+                    console.log(item)
+                    // todo : delete weight update weight
+                    deleteWeight(item.id)
+                }}
+                icon="delete"
+                text=""
+                styles={{ width: 40, padding: 0, margin: 0, height: 40, borderRadius: 50 }}
+
+            />
         </View>
     );
 
 
-    const logNewWeight = () => {
-        const newEntry = {
-            id: data.length.toString(),
-            weight: Math.floor(Math.random() * 50) + 50,
-            height: Math.floor(Math.random() * 50) + 150,
-        };
-        setData((prevData) => [...prevData, newEntry]);
+    const logNewWeight = async () => {
+        try {
+            const response = await fetch(`${defaultUrl}/weight`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authenticated}`
+                },
+                body: JSON.stringify({ weight, unit })
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to log weight');
+            }
+
+            const { success, message } = await response.json();
+            if (!success) {
+                throw new Error(message);
+            }
+
+            Alert.alert('Success', 'Weight logged successfully');
+            setWeight(null);
+            setIsVisible(false);
+            fetchWeights();
+        } catch (error) {
+            console.error('Error logging weight:', error);
+            Alert.alert('Error', error.message);
+        }
     };
     return (
         <View className="pr-8 pl-8 pt-4 pb-4" style={{ flex: 1 }}>
+            <ModalSlideUp
+                isVisible={isVisible}
+                onClose={() => setIsVisible(false)}
+                props={{
+                    title: "Add new weight",
+                }}
+            >
+
+                <View style={styles.inputContainer}>
+                    <Text style={{
+                        fontSize: 14,
+                        fontWeight: '600',
+                        color: colors.text,
+                        marginBottom: 8,
+                    }}>Weight:</Text>
+                    <TextInput
+                        style={{
+                            borderWidth: 1,
+                            borderColor: colors.tint,
+                            borderRadius: 8,
+                            padding: 12,
+                            fontSize: 16,
+                            color: colors.text,
+                            backgroundColor: 'transparent',
+                            marginBottom: 16,
+                        }}
+                        placeholder="Enter weight"
+                        keyboardType="numeric"
+                        value={weight}
+                        onChangeText={setWeight}
+                        selectTextOnFocus
+
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={{
+                        fontSize: 14,
+                        fontWeight: '600',
+                        color: colors.text,
+                        marginBottom: 8,
+                    }}>Unit:</Text>
+                    <Button
+                        text={unit === "kg" ? "kg" : "lbs"}
+                        onClick={() => setUnit(unit === "kg" ? "lbs" : "kg")}
+                    />
+                </View>
+                <View style={{ marginTop: 16 }}>
+                    <Button text="Log Weight" onClick={logNewWeight} />
+                </View>
+
+            </ModalSlideUp>
             <View style={{ flex: 1 }}>
                 <FlatList
-                    data={data}
+                    data={weights}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id}
                     onEndReachedThreshold={0.5}
@@ -47,7 +183,7 @@ export default function BodyWeightTrackingScreen() {
                     // fetch another new data
                 }} */
                 />
-                <Button title="Log New Weight" onPress={logNewWeight} />
+                <Button text="Log New Weight" onClick={() => { setIsVisible(true) }} />
             </View>
         </View>
     );
