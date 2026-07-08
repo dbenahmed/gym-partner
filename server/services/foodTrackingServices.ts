0baacdf829425612fafe34ae5ee0640bda45f5f5
Djilali Benahmed <dbenahmedd@gmail.com>
2026-07-08 21:55:37 +0100

    refactor<tracking food intakes feature> : splitting the controller code into controller, services, repositories

---
+++ server/services/foodTrackingServices.ts
@@ -0,0 +1,52 @@
+import { findMealsByDateAndUser, findFoodById, insertMealLog, findMealLogById, updateMealLog, deleteMealLog } from '../repositories/foodTrackingRepositories.js';
+
+export const getMealsService = async (date: string, userId: number) => {
+    const foundMeals = await findMealsByDateAndUser(date, userId);
+    const foundMealsWithFood = await Promise.all(foundMeals.map(async (meal) => {
+        const food = await findFoodById(meal.foodId);
+        return { ...meal, food };
+    }));
+    return foundMealsWithFood;
+};
+
+export const addMealService = async (date: string, foodId: number, userId: number, description: string | undefined | null, servingSize: number) => {
+    const foundFood = await findFoodById(foodId);
+    if (!foundFood) {
+        throw new Error("Food Id is in valid ( food not found )");
+    }
+
+    const desc = description ?? "";
+    const inserts = {
+        date,
+        foodId: foundFood.id,
+        userId: userId,
+        description: desc,
+        servingsizeG: servingSize
+    };
+
+    const insertedFood = await insertMealLog(inserts);
+    return insertedFood;
+};
+
+export const updateMealService = async (mealId: number, servingsizeG: number) => {
+    const checkTheMeal = await findMealLogById(mealId);
+    if (checkTheMeal.length === 0) {
+        throw new Error("the session is not exist verify the id");
+    }
+    await updateMealLog(mealId, servingsizeG);
+};
+
+export const deleteMealService = async (mealId: number, userId: number) => {
+    const foundFoodArray = await findMealLogById(mealId);
+    const foundFood = foundFoodArray[0];
+    
+    if (!foundFood) {
+        throw new Error("unfound food");
+    }
+
+    if (foundFood.userId !== userId) {
+        throw new Error("not authorized");
+    }
+
+    await deleteMealLog(mealId);
+};
